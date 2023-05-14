9b09dcadd02ec5bc8d3588f9dae45c067db970d2
salindae25 <salindae25@gmail.com>
2023-05-14 13:17:11 +0530

    fix some issue in huddle landing page

--- src/HuddleHome/index.tsx
+++ src/HuddleHome/index.tsx
@@ -5,10 +5,10 @@ const HuddleHome = () => {
     return (
         <div className="flex min-h-screen w-screen flex-col items-center gap-6 bg-[var(--huddle-primary)] font-['Poppins'] text-white ">
             <div
-                className={`flex min-h-screen w-full items-center gap-12 bg-cover
-bg-no-repeat px-8
+                className={`flex min-h-screen w-full items-center gap-12 bg-[url('/images/huddle/bg-mobile.svg')]
+bg-cover bg-no-repeat
+px-8
 calculator-app-mobile:flex-col
-calculator-app-mobile:bg-[url('/images/huddle/bg-mobile.svg')]
  calculator-app-desktop:gap-[40px] 
 calculator-app-desktop:bg-[url('/images/huddle/bg-desktop.svg')] calculator-app-desktop:px-16`}
             >
@@ -20,7 +20,7 @@ calculator-app-desktop:bg-[url('/images/huddle/bg-desktop.svg')] calculator-app-
                         className="mt-8 self-start"
                     />
                     <div className="flex flex-col gap-6 calculator-app-mobile:items-center calculator-app-desktop:items-start">
-                        <h3 className="text-xl font-semibold calculator-app-mobile:text-center">
+                        <h3 className="text-center text-xl font-semibold calculator-app-desktop:text-left calculator-app-desktop:text-4xl">
                             Build The Community
                             <br />
                             Your Fans Will Love
@@ -31,7 +31,11 @@ calculator-app-desktop:bg-[url('/images/huddle/bg-desktop.svg')] calculator-app-
                             connections with your users as you engage in genuine
                             discussion. Register
                         </p>
-                        <button className="shadow-[var(--huddle-primary) ]  w-[200px] rounded-3xl bg-white py-4 text-center text-[var(--huddle-primary)] shadow-lg">
+                        <button
+                            className="shadow-[var(--huddle-primary) ]  
+              w-[200px] rounded-3xl
+              bg-white py-4 text-center text-[var(--huddle-primary)] shadow-lg hover:bg-[var(--huddle-secondary)] hover:text-white"
+                        >
                             Register
                         </button>
                     </div>
